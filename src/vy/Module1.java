9ac736a8c9216a9aac10998ceec365ff26ff2ad8
maaravin <maaravin@LIN24008358.corp.capgemini.com>
2022-03-15 16:41:38 +0530

    First commit

---
+++ src/vy/Module1.java
@@ -0,0 +1,17 @@
+package vy;
+
+public class Module1 {
+
+	public static void main(String[] args) {
+		int a = 12;
+		if ( a==12) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+			
+
+	}
+
+}
